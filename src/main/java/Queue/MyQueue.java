a09eba40c3ae4e4b8e6168d624a1cbc4760656ee
Philipp <truphc16@htl-kaindorf.ac.at>
2019-05-03 12:23:58 +0200

    added Queue

---
+++ src/main/java/Queue/MyQueue.java
@@ -0,0 +1,27 @@
+import Queue.EmptyException;
+import Queue.FullException;
+import java.util.LinkedList;
+
+public class MyQueue<T> {
+
+    private final LinkedList<T> data = new LinkedList<>();
+    private int maxSize;
+
+    public MyQueue(int maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    public void put(T value) throws FullException {
+        if (data.size() == maxSize) {
+            throw new FullException();
+        }
+        data.add(value);
+    } 
+
+    public T get() throws EmptyException {
+        if (data.isEmpty()) {
+            throw new EmptyException();
+        }
+        return data.poll();
+    }
+}
